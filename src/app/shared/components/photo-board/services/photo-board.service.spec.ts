af86533fb75e3e42878188f45560c1d488cd2e9e
Matheus Alencar <matheusalencar6942@gmail.com>
2023-02-22 15:57:47 -0300

    testando lista de photos e seviços

---
+++ src/app/shared/components/photo-board/services/photo-board.service.spec.ts
@@ -0,0 +1,51 @@
+import { TestBed } from '@angular/core/testing';
+import { PhotoBoardService } from './photo-board.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+
+const mockedData = {
+  api: 'http://localhost:3000/photos',
+  data: [
+    {
+      id: 1,
+      description: 'example 1',
+      src: '',
+    },
+    {
+      id: 2,
+      description: 'example 2',
+      src: '',
+    },
+  ],
+};
+
+describe(PhotoBoardService.name, () => {
+  let service: PhotoBoardService;
+  let httpController: HttpTestingController;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PhotoBoardService],
+    }).compileComponents();
+
+    service = TestBed.inject(PhotoBoardService);
+    httpController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpController.verify();
+  });
+
+  it(`#${PhotoBoardService.prototype.getPhotos.name}
+  should return photos with description in uppercase`, (done) => {
+    service.getPhotos().subscribe((photos) => {
+      expect(photos[0].description).toBe('EXAMPLE 1');
+      expect(photos[1].description).toBe('EXAMPLE 2');
+      done();
+    });
+    httpController.expectOne(mockedData.api).flush(mockedData.data);
+  });
+});
